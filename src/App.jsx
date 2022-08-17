5a3b95ff26a356afc729dea388943dbf9c60ad1a
farai <47806090+ifarai@users.noreply.github.com>
2022-08-17 11:27:45 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './index.css';
+import abi from "./utils/WavePortal.json"
+
+export default function App() {
+
+	const [currentAccount, setCurrentAccount] = useState("")
+	const [message, setMessage] = useState("")
+	const [totalWaves, setTotalWaves] = useState(0)
+	const [allWaves, setAllWaves] = useState([])
+
+	const contractAddress = "0xA67Ba59788E2D8d341b52AA479f563BC6C17c9Dd"
+	const contractABI = abi.abi
+
+	const getAllWaves = async() => {
+		const { ethereum } = window;
+
+		try{
+			if (ethereum){
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+				const waves = await wavePortalContract.getAllWaves();
+				const wavesNumber = await wavePortalContract.getTotalWaves();
+
+				console.log(waves)
+				
+				let wavesArray = [];
+				waves.forEach( wave => {
+					wavesArray.push({
+						address: wave.waver,
+						timestamp : new Date(wave.timestamp * 1000),
+						message : wave.message
+					});
+				});
+
+				setAllWaves(wavesArray)
+				setTotalWaves(wavesNumber.toNumber())
+				console.log(wavesArray)
+				
+			}else{
+				console.log("You are not logged in with metamask")
+			}
+			
+			
+		}catch(error){
+			console.log(error)
+		}
+		
+	};
+
+	const checkIfWalletIsConnected = async () => {
+
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				console.log("Make sure that you are connected with metamask");
+			} else {
+				console.log("We have the ethereum object", ethereum)
+			}
+
+			//check wallet access authorisation
+
+			const accounts = await ethereum.request({ method: "eth_accounts" })
+
+			if (accounts.length !== 0) {
+				const account = accounts[0];
+				console.log("Found an authorised account", account)
+				setCurrentAccount(account)
+			} else {
+				console.log("No authorised accounts")
+			}
+
+		} catch (error) {
+			console.log(error)
+		}
+
+	}
+
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get some Metamask, even though Rainbow is better");
+				return;
+			}
+
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0])
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+
+	const wave = async () => {
+		try {
+			const { ethereum } = window; //injected by metamask window.
+
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+				let count = await wavePortalContract.getTotalWaves();
+				console.log("Retrieved total wave count...", count.toNumber());
+
+
+				//lets do the wave
+
+				const waveTxn = await wavePortalContract.wave(message, {gasLimit: 300000});
+				console.log("GM sent with the message %s", message)
+				console.log("Mining....", waveTxn.hash);
+
+				await waveTxn.wait();
+				console.log("Mined-----", waveTxn.hash);
+				setMessage("")
+
+				count = await wavePortalContract.getTotalWaves();
+				console.log("Retrieved total wave count...", count.toNumber());
+				setTotalWaves(count.toNumber())
+
+				let waves = await wavePortalContract.getAllWaves();
+				console.log("Retrieved these waves", waves);
+
+			} else {
+				console.log("Oh buddy, it does not look like you have the ethereum object")
+			}
+
+		} catch (error) {
+			console.log(error);
+		}
+
+
+	}
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+		getAllWaves();
+	}, []) //will only load once when the page is rendered,
+
+	return (
+			<div className="flex flex-col place-items-center max-w-full mt-16 space-y-4">
+	
+				<div className="flex flex-col place-items-center max-w-xl space-y-4">
+					<div className="text-4xl font-semibold text-center">
+						👋🏿 GM there mfer!
+	        </div>
+	
+					<div className="text-base text-center mb-4">
+						I am Farai, I work as a mechanical engineer but looking to transition to Web3. Have done some cool shits before!
+	        </div>
+					{!currentAccount && (
+						<button className="btn btn-wide" onClick={connectWallet}>
+							GM 👋🏿, Connect Wallet
+	        	</button>
+					) }	
+
+				</div>
+
+				{currentAccount && (
+
+				<div> 
+					<div className="form-control">
+						<label className="label">
+    					<span className="label-text">Write some GM message and wave!</span>
+  					</label> 
+					  <textarea onChange={(e)=> setMessage(e.target.value)} placeholder="Say GM!" className="textarea textarea-bordered mb-4"/>
+						<button className="btn btn-wide" onClick={wave}>
+						Wave at Me
+	        	</button>
+					</div>
+				</div>
+				)}
+
+
+				<div className="mt-4 text-2xl font-bold">
+					Following Mfers have said GM!
+				</div>
+
+				<div className="stats shadow">
+				  <div className="stat">
+				    <div className="stat-title">Total GMs 👋🏿</div>
+				    <div className="stat-value">{totalWaves}</div>
+				    <div className="stat-desc">Make number go 📈</div>
+				  </div>
+				</div>
+
+				
+				{allWaves.map((wave) => {
+				return (
+				<div> 
+					<div className="card w-96 bg-base-100 shadow-xl">
+					  <div className="card-body">
+					    <p className="text-sm font-extrabold truncate ...">From: {wave.address}</p>
+							<p className="font-bold truncate ...">{wave.timestamp.toString()}</p>
+					    <p className="text-base">{wave.message}</p>
+					  </div>
+				</div>
+				</div>
+				)})}
+		
+				
+				
+	</div>
+
+			
+	);
+}
---
+++ src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+//rainbow kit imports
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+//Rainbowkit setup
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.rinkeby, chain.arbitrum],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
+
+ReactDOM.render(
+	<WagmiConfig client={wagmiClient}>
+		<RainbowKitProvider chains={chains}>
+			<React.StrictMode>
+					<App />
+			</React.StrictMode>
+		</RainbowKitProvider>
+	</WagmiConfig>
+		,
+  document.getElementById('root')
+)
